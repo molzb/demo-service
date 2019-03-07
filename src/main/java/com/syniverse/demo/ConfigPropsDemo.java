f21ce315384b87ef8ca9d8808fbca8f2c01d0abc
Bernhard <Bernhard@DESKTOP-BERNHARD.Speedport_W723_V_Typ_A_1_01_018>
2019-03-07 10:35:57 +0100

    More Configuration

--- src/main/java/com/syniverse/demo/ConfigPropsDemo.java
+++ src/main/java/com/syniverse/demo/ConfigPropsDemo.java
@@ -1,5 +1,7 @@
 package com.syniverse.demo;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,9 +18,13 @@ public class ConfigPropsDemo {
 	private String name;
 	private String description;
 	private String version;
+	private int build;
+	
+	private List<String> servers;
 	
 	@PostConstruct
 	public void showInfoApp() {
-		System.out.println(name + "\n" + description);
+		System.out.printf("\tApp according to config: %s, Build %d%n \t%s%n", name, build, description);
+		System.out.println("\tServers according to config: " + servers == null ? "null" : servers.toString());
 	}
 }
