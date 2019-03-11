d28b2228bf8f9f1478e5108048fa99278cc70c24
Bernhard Molz <molzb2@gmx.de>
2019-03-11 15:32:56 +0100

    XML Configuration

--- src/main/java/com/syniverse/demo/config/ConfigPropsComponent.java
+++ src/main/java/com/syniverse/demo/config/ConfigPropsComponent.java
@@ -4,17 +4,29 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource(value="classpath:additional.properties")
 public class ConfigPropsComponent {
 	@Autowired
 	@Qualifier("appname")
 	private String appname;
+	
+	@Value("${earth.radius}")
+	private String earthRadius;
+	@Value("${earth.diameter}")
+	private String earthDiameter;
+	@Value("${earth.moons}")
+	private String earthMoons;
 
 	@PostConstruct
 	private void showAppname() {
 		System.out.printf("%s uses Singleton Bean 'appname': %s%n", getClass().getSimpleName(), appname);
+		System.out.println("These values come from additional.properties:");
+		System.out.printf("\t Earth radius:%s, diameter:%s, moons:%s%n", earthRadius, earthDiameter, earthMoons);
 	}
 
 }
---
+++ src/main/java/com/syniverse/demo/config/ConfigXmlDemo.java
@@ -0,0 +1,55 @@
+package com.syniverse.demo.config;
+
+import java.awt.Button;
+import java.awt.Point;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ImportResource({"classpath:applicationContext.xml"})
+@Component
+public class ConfigXmlDemo {
+	ConfigXmlDemo() {
+		System.setProperty("java.awt.headless", "false");	// Button crashes, if I don't do that
+	}
+	
+	// No qualifier needed, because the names are the same in Java and XML
+	@Autowired
+	private String canWeUseBothXmlAndProperties;
+	@Autowired
+	private String stringInBeansXml;
+	@Autowired
+	private String additionalXml;
+	
+	@Autowired
+	@Qualifier("qualifierDemo")
+	private String qualifierNeeded;
+	
+	@Autowired
+	private Point xmasPoint;
+	
+	@Autowired
+	private Button xmasButton;
+
+	@Autowired
+	private Button xmasButtonAlias;
+
+	@PostConstruct
+	private void showXmlBean() {
+		System.out.println("\t-------------------------------------");
+		System.out.println("\tCan we use both XML and properties? " + canWeUseBothXmlAndProperties);
+		System.out.println("\tValue of XML Bean: " + stringInBeansXml);
+		System.out.println("\tAnother XML: " + additionalXml);
+		System.out.println("\tExample with qualifier: " + qualifierNeeded);
+		System.out.println("\tExample with constructor: " + xmasPoint);
+		System.out.println("\tExample with properties: "  + xmasButton);
+		System.out.println("\tThe same object as 'alias': "  + xmasButtonAlias);
+		System.out.println("\t-------------------------------------");
+	}
+}
